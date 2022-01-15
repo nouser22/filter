d89d6fae6c35693ffddc853d0cbf2c32de36926f
nouser22 <97242117+nouser22@users.noreply.github.com>
2022-01-15 15:48:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload(){
+}
+function setup(){
+    canvas= createCanvas(300,300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose'.gotPoses);
+}
+function modelLoaded(){
+  console.log('Posenet loaded');
+}
+function draw(){
+image(video,0,0,300,300);
+}
+function take_snapshot(){
+  save("filterimage.png");
+}
